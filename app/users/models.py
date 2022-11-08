0de8ec8ae6542796994581a224b449451a195472
Alexis Michaltsis <a.michaltsis@gmail.com>
2022-11-08 12:22:12 +0100

    Only allow unique emails for user accounts

--- app/users/models.py
+++ app/users/models.py
@@ -1,5 +1,16 @@
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+from django.db.models import EmailField
 
 
 class CustomUser(AbstractUser):
-    pass
+    email = EmailField(
+        _('email address'),
+        blank=False,
+        error_messages={
+            'unique': _(
+                "A user with that email already exists."),
+        },
+        null=False,
+        unique=True,
+    )
