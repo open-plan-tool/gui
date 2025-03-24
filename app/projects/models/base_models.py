8fcc66ec4cea5b4fdf6ab830f21aa564314db678
pierre-francois.duc <pierre-francois.duc@rl-institut.de>
2025-03-24 01:44:31 +0100

    WIP soc initial

--- app/projects/models/base_models.py
+++ app/projects/models/base_models.py
@@ -362,7 +362,6 @@ class Timeseries(models.Model):
             self.end_date = self.compute_end_date_from_duration()
 
 
-
 class AssetType(models.Model):
     asset_type = models.CharField(
         max_length=30, choices=ASSET_TYPE, null=False, unique=True
@@ -447,7 +446,9 @@ class Asset(TopologyNode):
     input_timeseries_old = models.TextField(
         null=True, blank=False
     )  # , validators=[validate_timeseries])
-    input_timeseries = models.ForeignKey(Timeseries, on_delete=models.CASCADE, null=True, blank=False)
+    input_timeseries = models.ForeignKey(
+        Timeseries, on_delete=models.CASCADE, null=True, blank=False
+    )
     crate = models.FloatField(
         null=True, blank=False, default=1, validators=[MinValueValidator(0.0)]
     )
@@ -466,6 +467,13 @@ class Asset(TopologyNode):
         blank=False,
         validators=[MinValueValidator(0.0), MaxValueValidator(1.0)],
     )
+
+    #
+    # soc_initial = models.FloatField(
+    #     null=True,
+    #     blank=False,
+    #     validators=[MinValueValidator(0.0), MaxValueValidator(1.0)],
+    # )
     dispatchable = models.BooleanField(
         null=True, blank=False, choices=TRUE_FALSE_CHOICES, default=None
     )
