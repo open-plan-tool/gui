c6698ae75f6264d6bf1c28745984acde985551ff
pierre-francois.duc <pierre-francois.duc@rl-institut.de>
2025-08-28 23:56:07 +0200

    Add port to the AssetType csv
    This is linked via a ForeignKey inside the database

--- app/projects/management/commands/update_assettype.py
+++ app/projects/management/commands/update_assettype.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand, CommandError
 import pandas as pd
+import json
 from projects.models import *
 
 
@@ -17,13 +18,32 @@ class Command(BaseCommand):
 
         df = pd.read_csv("static/assettypes_list.csv")
         assets = df.to_dict(orient="records")
+
         for asset_params in assets:
             qs = AssetType.objects.filter(asset_type=asset_params["asset_type"])
+            asset_ports = asset_params.pop("ports", None)
 
             if qs.exists() is False:
-
                 new_asset = AssetType(**asset_params)
                 new_asset.save()
             else:
                 if update_assets is True:
                     qs.update(**asset_params)
+
+            asset_type = AssetType.objects.get(asset_type=asset_params["asset_type"])
+
+            if asset_ports is not None:
+
+                # TODO delete potential existing connexion ports
+                print()
+                asset_type.ports.all().delete()
+
+                asset_ports = json.loads(asset_ports.replace("'", '"'))
+                for key, label in asset_ports.items():
+                    direction, num = key.split("_")
+                    port, created = ConnectionPort.objects.get_or_create(
+                        direction=direction,
+                        num=int(num),
+                        label=label,
+                        asset_type=asset_type,
+                    )
