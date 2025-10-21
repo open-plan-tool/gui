294b1a083014dae77532c2467b793a7dfb4fc558
paulapreuss <paulavpreuss@outlook.com>
2025-10-21 09:07:59 +0200

    Make linter happy

--- app/projects/migrations/0027_assign_timeseries_asset_type.py
+++ app/projects/migrations/0027_assign_timeseries_asset_type.py
@@ -1,4 +1,5 @@
 from django.db import migrations
+
 # This migration assigns timeseries asset types to existing timeseries in the database
 ALLOWED = {
     "demand",
@@ -12,14 +13,13 @@ ALLOWED = {
     "solar_thermal_plant",
 }
 
+
 def forwards(apps, schema_editor):
     Asset = apps.get_model("projects", "Asset")
     Timeseries = apps.get_model("projects", "Timeseries")
 
-    qs = (
-        Asset.objects
-        .select_related("asset_type", "input_timeseries")
-        .only("id", "asset_type__asset_type", "input_timeseries_id")
+    qs = Asset.objects.select_related("asset_type", "input_timeseries").only(
+        "id", "asset_type__asset_type", "input_timeseries_id"
     )
     to_update = []
     for asset in qs.iterator():
@@ -31,7 +31,9 @@ def forwards(apps, schema_editor):
         # Check that the asset type is in the allowed values
         asset_type = getattr(asset.asset_type, "asset_type", None)
         if asset_type not in ALLOWED:
-            raise ValueError(f"Asset type {asset_type} not in allowed timeseries assets")
+            raise ValueError(
+                f"Asset type {asset_type} not in allowed timeseries assets"
+            )
 
         # Add object to list to be updated
         to_update.append((ts_id, asset_type))
@@ -39,6 +41,7 @@ def forwards(apps, schema_editor):
     # Batch update timeseries instances
     if to_update:
         from itertools import islice
+
         CHUNK = 1000
         it = iter(to_update)
         while True:
@@ -48,9 +51,11 @@ def forwards(apps, schema_editor):
             objs = [Timeseries(id=ts_id, asset_type=value) for ts_id, value in chunk]
             Timeseries.objects.bulk_update(objs, ["asset_type"], batch_size=CHUNK)
 
+
 def backwards(apps, schema_editor):
     Timeseries = apps.get_model("projects", "Timeseries")
     Timeseries.objects.update(asset_type=None)
+
 
 class Migration(migrations.Migration):
     dependencies = [
