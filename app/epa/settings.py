25435f6fbcd2e5f3054b275c98ea3c2946fdc40b
paulapreuss <paulavpreuss@outlook.com>
2026-02-12 14:00:13 +0100

    Add security settings flagged by manage.py check --deploy

--- app/epa/settings.py
+++ app/epa/settings.py
@@ -171,8 +171,6 @@ LOGIN_URL = "login"
 LOGIN_REDIRECT_URL = "project_search"
 LOGOUT_REDIRECT_URL = "home"
 
-CRISPY_TEMPLATE_PACK = "bootstrap4"
-
 # Please note, we don't use Django's internal email system,
 # we implement our own, using exchangelib
 USE_EXCHANGE_EMAIL_BACKEND = env.bool("USE_EXCHANGE_EMAIL_BACKEND", default=True)
@@ -260,3 +258,28 @@ Q_CLUSTER = {
     "queue_limit": 50,
     "orm": "default",
 }
+
+# SECURITY
+# https://docs.djangoproject.com/en/dev/ref/settings/#secure-ssl-redirect
+SECURE_SSL_REDIRECT = env.bool("DJANGO_SECURE_SSL_REDIRECT", default=True)
+# https://docs.djangoproject.com/en/dev/ref/settings/#session-cookie-secure
+SESSION_COOKIE_SECURE = True
+# https://docs.djangoproject.com/en/dev/ref/settings/#session-cookie-name
+SESSION_COOKIE_NAME = "__Secure-sessionid"
+# https://docs.djangoproject.com/en/dev/ref/settings/#csrf-cookie-secure
+CSRF_COOKIE_SECURE = True
+# https://docs.djangoproject.com/en/dev/ref/settings/#secure-hsts-seconds
+# TODO: set this to 60 seconds first and then to 518400 once you prove the former works
+SECURE_HSTS_SECONDS = 60
+# https://docs.djangoproject.com/en/dev/ref/settings/#secure-hsts-include-subdomains
+SECURE_HSTS_INCLUDE_SUBDOMAINS = env.bool(
+    "DJANGO_SECURE_HSTS_INCLUDE_SUBDOMAINS",
+    default=True,
+)
+# https://docs.djangoproject.com/en/dev/ref/settings/#secure-hsts-preload
+SECURE_HSTS_PRELOAD = env.bool("DJANGO_SECURE_HSTS_PRELOAD", default=True)
+# https://docs.djangoproject.com/en/dev/ref/middleware/#x-content-type-options-nosniff
+SECURE_CONTENT_TYPE_NOSNIFF = env.bool(
+    "DJANGO_SECURE_CONTENT_TYPE_NOSNIFF",
+    default=True,
+)
