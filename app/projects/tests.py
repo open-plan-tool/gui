add23017c26818865740cbedc201e05776b06eb0
josihoppe <116898820+josihoppe@users.noreply.github.com>
2026-04-21 13:42:08 +0200

    added test for checking that headers are not accepted

--- app/projects/tests.py
+++ app/projects/tests.py
@@ -389,3 +389,16 @@ class UploadTimeseriesTest(TestCase):
             }
             response = self.client.post(self.post_url, data, format="multipart")
             self.assertEqual(response.status_code, 422)
+
+    def test_load_demand_csv_double_decimal_point_with_point_header_error(self):
+        with open("./test_files/test_ts_csv_semicolon_point_header.csv") as fp:
+            data = {
+                "name": "Test_input_timeseries",
+                "pos_x": 0,
+                "pos_y": 0,
+                "input_timeseries_scalar": "",
+                "input_timeseries_select": "",
+                "input_timeseries_file": fp,
+            }
+            response = self.client.post(self.post_url, data, format="multipart")
+            self.assertEqual(response.status_code, 422)
