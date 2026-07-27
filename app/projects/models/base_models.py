856e81d65b29385b25a50ac4419bf7ddaa0c64bc
pierre-francois.duc <pierre-francois.duc@rl-institut.de>
2026-07-27 12:03:48 +0200

    Adapt heat_pump form

--- app/projects/models/base_models.py
+++ app/projects/models/base_models.py
@@ -1270,6 +1270,7 @@ class HeatPump(Asset):
                 min=1,
                 param_name="cop",
                 label=_("COP"),
+                help_text="This is the custom help text for COP",
             )
         }
 
