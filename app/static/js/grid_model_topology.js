d39a2ee3378fbd8b80f8395317095a835f516259
pierre-francois.duc <pierre-francois.duc@rl-institut.de>
2026-07-27 12:17:09 +0200

    Fix COPCalculator link

--- app/static/js/grid_model_topology.js
+++ app/static/js/grid_model_topology.js
@@ -836,7 +836,7 @@ function computeCOP(event){
             // close the cop area
             copCollapse.hide();
 
-            efficiencyDOM = guiModalDOM.querySelector('input[name="efficiency_scalar"]');
+            efficiencyDOM = guiModalDOM.querySelector('input[name="cop_scalar"]');
             if(efficiencyDOM){
                 efficiencyDOM.value = jsonRes.cops; efficiencyDOM.dispatchEvent(new Event('change'));
             }
